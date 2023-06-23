1a96d0f36f858940eccca3ac8a14a94f438b9165
Divya R <kwrdivyaraj100@gmail.com>
2023-06-23 20:29:43 +0530

    Do while loop program

---
+++ 12basic/LoopsDemoThree.java
@@ -0,0 +1,15 @@
+class LoopsDemoThree
+{
+ public static void main(String args[])
+ {
+ int i = 0;
+
+ do
+ {
+ System.out.println("My Nmae is Balaji");
+ i++;
+ }
+ while(i<5);
+ System.out.println("Outside do-while loop");
+ }
+ }
