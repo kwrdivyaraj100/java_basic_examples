04e2e7770e077a1fa9e3434d4fd8e278454cff02
Divya R <kwrdivyaraj100@gmail.com>
2023-06-23 20:56:50 +0530

    Enhanced for loop structure

---
+++ 12basic/LoopsDemoFive.java
@@ -0,0 +1,13 @@
+class LoopsDemoFive
+{
+ public static void main(String args[])
+ {
+ int z[] = {55, 65, 75, 85, 95};
+
+ for(int i = 0; i < z.length; i++)
+ {
+ System.out.println("Value of z["+i+"] is: " +z[i]);
+ }
+ System.out.println("Outside while loop");
+ }
+ }
