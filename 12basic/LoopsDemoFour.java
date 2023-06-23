86b4fa468378b9add99d3811790b8555f26405ee
Divya R <kwrdivyaraj100@gmail.com>
2023-06-23 20:36:35 +0530

    logic balancer in loops

---
+++ 12basic/LoopsDemoFour.java
@@ -0,0 +1,13 @@
+class LoopsDemoFour
+{
+ public static void main(String args[])
+ {
+ int i = 0;
+ while(i<5)
+ {
+ System.out.println("My Name is Balaji");
+ i++;
+ }
+ System.out.println("Outside while loop");
+ }
+ }
